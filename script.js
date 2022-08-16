90d3436f50c27c628728fb46793a8abef7a4e890
Alexandr Zabolotny <saanches7@gmail.com>
2022-08-16 17:00:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,17 +27,16 @@ findRowCode('.text');
 function returnFindRowCode(textContainers) {
   const text = document.querySelector(textContainers);
   const reg = /[\_\/\-\\]|.jsp|.html|.css|.js|.txt|.png|.jpg|.svg/gm;
-  let changeText = '';
 
   const arr = text.textContent.trim().split(' ');
   arr.find((i, index) => { 
     if(i.match(reg)) {
       arr[index] = `<div style="color: #CA0000; font-size: 13px;">${arr[index]}</div>`;
     }
-    changeText = arr.join(' ');
   });
-  return changeText;
+  return arr.join(' ');
 }
+
 
 document.querySelector('.text2').innerHTML = returnFindRowCode('.text2');
 document.querySelector('.text3').innerHTML = returnFindRowCode('.text3');
